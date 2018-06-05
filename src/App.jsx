65ba77fbd7d6d7e4f61bf04033e677ec723e38b8
jpelliza <jpelliza.gcba@gmail.com>
2018-06-05 17:41:26 -0300

    - showing title app countdown through state

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2017'
+        }
+    }
+
+    changeDeadline(){
+        this.setState({deadline: 'November 25, 2017'});
+    }
+
+    render(){
+        return(
+            <div className="App">
+                <div className="App-title" >Countdown to {this.state.deadline}</div>
+                <div>
+                    <div className="Clock-days">14 days</div>
+                    <div className="Clock-hours">30 hours</div>
+                    <div className="Clock-minutes">15 minutes</div>
+                    <div className="Clock-seconds">20 seconds</div>
+                </div>
+                <div>
+                    <input type="text" placeholder="new date"/>
+                    <button onClick={() => this.changeDeadline()} >Submit</button> {/* Funcion anonima "() =>" para que se ejecute solo 1 vez y evitar loops */}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
